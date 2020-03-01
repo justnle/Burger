0a407026d3fb1ac76204c6611661623aede5a4f5
Justin <twoplatinumchainz@gmail.com>
2020-03-01 11:12:49 -0800

    remake-burger click function

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -17,16 +17,17 @@ $(() => {
     });
   });
 
-  $(`.create-burger`).on(`submit`, event => {
-    event.preventDefault();
+  $(`.remake-burger`).on(`click`, function(event) {
+    const id = $(this).data(`id`);
+    const newDevoured = false;
 
-    const newBurger = {
-      name: $(`#burger`).val().trim()
+    const newDevouredState = {
+      devoured: newDevoured
     };
 
-    $.ajax(`/api/burgers`, {
-      type: `POST`,
-      data: newBurger
+    $.ajax(`/api/burgers/${ id }`, {
+      type: `PUT`,
+      data: newDevouredState
     }).then(() => {
       location.reload();
     });
@@ -37,6 +38,21 @@ $(() => {
 
     $.ajax(`/api/burgers/${ id }`, {
       type: `DELETE`
+    }).then(() => {
+      location.reload();
+    });
+  });
+
+  $(`.create-burger`).on(`submit`, event => {
+    event.preventDefault();
+
+    const newBurger = {
+      name: $(`#burger`).val().trim()
+    };
+
+    $.ajax(`/api/burgers`, {
+      type: `POST`,
+      data: newBurger
     }).then(() => {
       location.reload();
     });
