86b40c35564bc9df56126c0da39a63c32c9b2b24
Justin <twoplatinumchainz@gmail.com>
2020-02-29 22:48:58 -0800

    put and posts working

---
+++ public/assets/js/burger.js
@@ -0,0 +1,37 @@
+'use strict';
+
+$(() => {
+  $(`.change-devour`).on(`click`, function(event) {
+    const id = $(this).data(`id`);
+    const newDevoured = true;
+
+    const newDevouredState = {
+      devoured: newDevoured
+    };
+
+    $.ajax(`/api/burgers/${ id }`, {
+      type: `PUT`,
+      data: newDevouredState
+    }).then(() => {
+      console.log(`changed devoured to`, newDevoured);
+      location.reload();
+    });
+  });
+
+  $(`.create-burger`).on(`submit`, event => {
+    event.preventDefault();
+
+    const newBurger = {
+      name: $(`#burger`).val().trim()
+    };
+
+    $.ajax(`/api/burgers`, {
+      type: `POST`,
+      data: newBurger
+    }).then(() => {
+      console.log(`Added a new burger!`);
+      location.reload();
+    });
+  });
+});
+
